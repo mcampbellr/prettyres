aa750b0df34d795b086e45d22e0f653d93565493
Mc <mariocampbellr@gmail.com>
2020-05-03 01:35:24 -0600

    Pretty response ready to use

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,17 @@ interface JsonRes {
 }
 
 class Reply {
-  public static send (res: Response, code: number, data: object): void // eslint-disable-line 
-  public static send (res: Response, code: number, message: string, data: object): void// eslint-disable-line
-  public static send (res: Response, code: number, message: string | object, data?: object): void {// eslint-disable-line
+  public static send (res: Response, code: number, data: object | any): void // eslint-disable-line 
+  public static send (res: Response, code: number, message: string, data: object | any): void// eslint-disable-line
+  public static send (res: Response, code: number, message: string | object, data?: object | any): void {// eslint-disable-line
     let jsonRes: JsonRes
     let type: string = this.setInfo(code)
+    let error: boolean | undefined = this.setError(code)
 
     jsonRes = {
       code,
-      type
-    }
-
-    // @ts-ignore
-    if (type !== 'informational' || type !== 'success') {
-      jsonRes.error = true
+      type,
+      error
     }
 
     if (typeof message === 'string') jsonRes.message = message
@@ -32,6 +29,20 @@ class Reply {
     if (data) jsonRes.data = data
 
     res.status(code).json(jsonRes)
+  }
+
+  static setError (code: number): boolean | undefined {
+    const first: number = Number(String(code).charAt(0))
+    switch (first) {
+      case 1: 
+      case 2:
+          return undefined
+      case 3:
+      case 4:
+      case 5:
+      default:
+        return true
+    }
   }
 
   static setInfo (code: number): string {
